b3bfc7a7a09a90d7a8f8b59d9b2748b3b2aa9fc7
lee <545490136@qq.com>
2015-12-01 10:17:02 +0800

    day2-am1

---
+++ app/src/main/java/app/leeweather/com/gittest/Province.java
@@ -0,0 +1,33 @@
+package app.leeweather.com.gittest;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by leexumin on 2015/12/1.
+ */
+public class Province {
+
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id= id;
+    }
+    public String getProvinceName (){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+    public String getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(String provinceCode){
+        this.provinceCode = provinceCode;
+    }
+}
