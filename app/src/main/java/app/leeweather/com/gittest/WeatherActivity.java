f3e7efad663910f296bb40c4501ce7881b2f9252
lee <545490136@qq.com>
2015-12-04 11:13:32 +0800

    day5-am

--- app/src/main/java/app/leeweather/com/gittest/WeatherActivity.java
+++ app/src/main/java/app/leeweather/com/gittest/WeatherActivity.java
@@ -1,19 +1,22 @@
 package app.leeweather.com.gittest;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
  * Created by leexumin on 2015/12/3.
  */
-public class WeatherActivity extends Activity {
+public class WeatherActivity extends Activity implements View.OnClickListener{
     private LinearLayout weatherInfoLayout;
     //用于显示城市名字
     private TextView cityNameText;
@@ -26,6 +29,10 @@ public class WeatherActivity extends Activity {
     private TextView temp2Text;
     //用于显示当前日期
     private TextView currentDataText;
+    //切换城市按钮
+    private Button switchCity;
+    //更新天气按钮
+    private Button refreshWeather;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -41,10 +48,11 @@ public class WeatherActivity extends Activity {
         currentDataText= (TextView)findViewById(R.id.current_data);
         String countyCode =getIntent().getStringExtra("county_code");
 
+
        if (!TextUtils.isEmpty(countyCode)){
             //有县级代号时就去查询天气
 
-            publishText.setText("正在很努力的同步...");
+            publishText.setText("正在很努力地同步...");
             weatherInfoLayout.setVisibility(View.INVISIBLE);
             cityNameText.setVisibility(View.INVISIBLE);
             queryWeatherCode(countyCode);
@@ -54,21 +62,50 @@ public class WeatherActivity extends Activity {
         //没有县级代号时就直接查询显示本地天气
             showWeather();
         }
+        switchCity = (Button) findViewById(R.id.switch_city);
+        refreshWeather =(Button)findViewById(R.id.refresh_weather);
+        switchCity.setOnClickListener(this);
+        refreshWeather.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.switch_city:
+              Intent intent = new Intent(this,ChooseAreaActivity.class);
+                intent.putExtra("from_weather_activity", true);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.refresh_weather:
+                   publishText.setText("正在很努力的同步...");
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+                String weatherCode =  prefs.getString("weather_code","");
+                if (!TextUtils.isEmpty(weatherCode)){
+                    queryWeatherInfo(weatherCode);
+                }
+                break;
+            default:
+                break;
+
+        }
     }
     /*
     查询县级对应代号的天气代号
      */
     private void queryWeatherCode (String countyCode){
 
-        String address = "http://www.weather.com.cn/data/list3/city"+ countyCode + ".xml";
+        String address = "http://www.weather.com.cn/data/list3/city" +  countyCode + ".xml";
         queryFromServer(address,"countyCode");
 
 
 
     }
-    //查询天气代号所对应的天气
+    /*
+    查询天气代号所对应的天气
+    * */
     private void queryWeatherInfo(String weatherCode){
-        String address = "http://www.weather.com.cn/data/cityinfo/"+weatherCode + ".html";
+        String address = "http://www.weather.com.cn/data/cityinfo/" + weatherCode + ".html";
+        queryFromServer(address,"weatherCode");
     }
     /*根据传入的地址和类型，去向服务器查询天气的代号或天气信息
 
